c18194b531bb7012862e7a8cac26f36486c569ac
Rushikesh Jadhav <rajadhav147@gmail.com>
2025-10-17 22:38:15 +0530

    completed Herobanner frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Route, Routes} from "react-router-dom";
+import { Box } from "@mui/material";
+
 import "./App.css";
+import ExerciseDetail from './pages/ExerciseDetail.js';
+import Home from './pages/Home.js';
+import Navbar from './components/Navbar.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Box width="400px" sx={{width: {xl:"1488px"}}} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   )
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { Box } from "@mui/material";
+
+
+import Exercises from "../components/Exercises.js";
+import HeroBanner from "../components/HeroBanner.js";
+import SearchExercises from "../components/SearchExercises.js";
+
+const Home = () => {
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  )
+}
+
+export default Home
